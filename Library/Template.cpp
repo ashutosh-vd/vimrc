dfe6abfa3ecedf1517dded199eb296bda885d825
ashutosh-vd <144103627+ashutosh-vd@users.noreply.github.com>
2024-08-10 06:12:18 +0530

    Add files via upload

---
+++ Library/Template.cpp
@@ -0,0 +1,24 @@
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
+#define int long long 
+#define endl "\n"
+using namespace std;
+const int INF = 1e18+10;
+const int MOD = 1e9 +7;
+
+inline void solve() {
+	
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio(false), cin.tie(nullptr);
+	int T = 1;
+	cin >> T;
+
+	while (T--)
+		solve();
+	
+ return 0;
+}
