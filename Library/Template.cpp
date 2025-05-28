e68e263cd7f063788060114e2d20089a3e9af70e
ashutosh-vd <ashutoshsensei09@gmail.com>
2025-05-28 22:06:13 +0530

    Update Template.cpp

--- Library/Template.cpp
+++ Library/Template.cpp
@@ -1,12 +1,22 @@
 #pragma GCC optimize ("O3")
-#pragma GCC target ("sse4")
+// #pragma GCC target ("sse4")
 
 #include <bits/stdc++.h>
+#ifdef DEBUG
+#include "../algoCOM/dbg.h"
+#else
+#define dbg(x...) 42
+#endif
+
 #define int long long 
 #define endl "\n"
+#define yes "YES"
+#define no "NO "
 using namespace std;
 const int INF = 1e18+10;
 const int MOD = 1e9 +7;
+
+const int N = 1e5+10;
 
 inline void solve() {
 	
